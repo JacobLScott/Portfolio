fa75bc5de73524260238b1dcf0306e0bb1041103
SleebyWeeby <100809654+SleebyWeeby@users.noreply.github.com>
2025-05-26 15:00:38 -0400

    ai changes for navbar

---
+++ portfolio-webroot/components/navbar.js
@@ -0,0 +1,40 @@
+class NavBar extends HTMLElement {
+    constructor() {
+        super();
+        
+        this.innerHTML = `
+            <div id="navbar">
+                <ul id="top-menu">
+                    <li id="name"><a href="index.html"><strong>Jacob Scott</strong></a></li>
+                    <button class="menu-toggle" aria-label="Toggle navigation menu">☰</button>
+                    <li><a href="">Professional Projects</a>
+                        <ul class="sub-menu">
+                            <li><a href="page_coming_soon.html">Unchained</a></li>
+                            <li><a href="page_bugnauts.html">Bugnauts!</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">Personal Projects</a>
+                        <ul class="sub-menu">
+                            <li><a href="page_coming_soon.html">Level Design</a></li>
+                            <li><a href="page_coming_soon.html">Fire Emblem Mods</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="page_resume.html">Resume</a></li>
+                    <li><a href="page_coming_soon.html">Contact</a></li>
+                </ul>
+            </div>
+        `;
+
+        // Add event listener for menu toggle
+        const menuToggle = this.querySelector('.menu-toggle');
+        const topMenu = this.querySelector('#top-menu');
+        
+        if (menuToggle && topMenu) {
+            menuToggle.addEventListener('click', () => {
+                topMenu.classList.toggle('active');
+            });
+        }
+    }
+}
+
+customElements.define('nav-bar', NavBar);
